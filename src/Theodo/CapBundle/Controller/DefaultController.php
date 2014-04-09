d0b188f86056b37bc750a72a192eac696c3bcf0f
Guillermo Rodríguez Peinado <grp@mbpdeguillermo.theodo.lan>
2014-04-09 10:34:46 +0200

    error to make NewRelic alerts

--- src/Theodo/CapBundle/Controller/DefaultController.php
+++ src/Theodo/CapBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        theodo(); //erreur pour verifier new Relic
         return $this->render('CapBundle:Default:index.html.twig', array('name' => $name));
     }
 }
