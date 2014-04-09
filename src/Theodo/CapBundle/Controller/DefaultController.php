f4ed448b6c6a3ce721fcdd183f74b567306812fa
Guillermo Rodríguez Peinado <grp@mbpdeguillermo.theodo.lan>
2014-04-09 11:26:39 +0200

    No errors!

--- src/Theodo/CapBundle/Controller/DefaultController.php
+++ src/Theodo/CapBundle/Controller/DefaultController.php
@@ -8,7 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        theodo(); //erreur pour verifier new Relic
         return $this->render('CapBundle:Default:index.html.twig', array('name' => $name));
     }
 }
